4fbad4125a6baf01735b39f41ebab4e936757faf
vidkyadhipradana2004@mail.ugm.ac.id <praktikan@SV-EIHU112-005.local>
2025-08-21 10:06:04 +0700

    update button

--- app/src/main/java/com/example/helloworldapp/MainActivity.kt
+++ app/src/main/java/com/example/helloworldapp/MainActivity.kt
@@ -2,21 +2,50 @@ package com.example.helloworldapp
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.helloworldapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var number=0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            txtNumber.text = number.toString()
+            btnCount.setOnClickListener{
+                number++
+                txtNumber.text=number.toString()
+            }
+
+            btnMin.setOnClickListener {
+                number--
+                txtNumber.text=number.toString()
+            }
+
+            btnToast.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Count $number",
+                    Toast.LENGTH_SHORT).show()
+            }
+
         }
+
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
 
         Log.d("CLOG", "onCrete: log debug")
         Log.e("CLOG", "onCrete: log error")
