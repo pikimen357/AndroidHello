4371b491ccb744ab2880e365ce3d04a402c59c2e
vidkyadhipradana2004@mail.ugm.ac.id <praktikan@SV-EIHU112-005.local>
2025-08-21 09:35:08 +0700

    update

--- app/src/main/java/com/example/helloworldapp/MainActivity.kt
+++ app/src/main/java/com/example/helloworldapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.helloworldapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Log.d("CLOG", "onCrete: log debug")
+        Log.e("CLOG", "onCrete: log error")
+        Log.i("CLOG", "onCrete: log info")
+        Log.w("CLOG", "onCrete: log warning")
+
     }
 }
